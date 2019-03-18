85875635230b1350865d22c611a60caf13ec4fd2
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-18 05:52:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(string)
   array = string.split(" ") 
   test_array = []
-  array.collect do|string| 
+  array.collect do |string| 
     test_array << string.reverse
   end
   test_array.join(" ")
